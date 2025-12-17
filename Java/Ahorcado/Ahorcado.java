a8f13fa8afa533d55c2dfa6b8cdd8b701694a970
Ossel18 <osantamariab01@educantabria.es>
2025-12-17 19:39:02 +0100

    Update Ahorcado.java
    Actualización del primer día del Ahorcado.java

--- Java/Ahorcado/Ahorcado.java
+++ Java/Ahorcado/Ahorcado.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.minireto;
 
+import java.util.Scanner;
+
 /**
  *
  * @author DAW120
@@ -11,6 +13,60 @@ package com.mycompany.minireto;
 public class Ahorcado {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner entrada = new Scanner(System.in);
+        
+        boolean terminado = false;
+        
+       
+        
+        String[] palabras = {"PERRO"};
+        
+        for(int i =  0; i < palabras[0].length(); i++){
+            System.out.print("_ ");
+        }
+        System.out.printf("\n");
+        
+         while(!terminado){
+             
+             String[] letra = new String[palabras.length + 6];
+             int contadorletra = 0;
+         
+        System.out.println("Introduce una letra: ");
+         letra[contadorletra] = entrada.next().toUpperCase().substring(0, 1);
+        
+        
+        
+        String[] letras = new String[palabras[0].length()];
+        
+         for(int i =  0; i < palabras[0].length(); i++){
+           
+             if(i == palabras[0].length()){
+                 letras[i] = palabras[0].substring(i);
+             }else{
+                 letras[i] = palabras[0].substring(i, i + 1);
+             }
+             
+         }
+        
+        boolean acierto = false;
+        
+        for(int i =  0; i < palabras[0].length(); i++){
+            if(letra[contadorletra].equalsIgnoreCase(letras[i])){
+                System.out.printf("%s ", letra );
+                acierto = true;
+            }else{
+                System.out.printf("_ ");
+            }
+        }
+            System.out.print("\n");
+            int vidas = 6;
+        if(!acierto){
+            vidas--;
+            System.out.println("Te quedan "+ vidas +" vidas");
+        }
+            
+        
+        }
+        
     }
 }
